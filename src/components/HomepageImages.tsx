cd34e40f19eb669d8a1e3fe52f42a553f642d945
Joelle Plow <joelleplow@aol.com>
2021-07-26 23:21:07 +0100

    Add support for captions, and other changes

--- src/components/HomepageImages.tsx
+++ src/components/HomepageImages.tsx
@@ -1,54 +1,52 @@
-import React, {FunctionComponent} from 'react';
-import ImageGrid from './ImageGrid'
+import React, { FunctionComponent } from "react";
+import ImageGrid from "./ImageGrid";
 
-import Img1 from '../img/homepage-images/3.tiff.png'
-import Img2 from '../img/homepage-images/cowl.tiff.png'
-import Img3 from '../img/homepage-images/veil.tiff.png'
-import Img4 from '../img/homepage-images/IMG_1640.jpg.png'
-import Img5 from '../img/homepage-images/IMG_1722.jpg.png'
-import Img6 from '../img/homepage-images/IMG_3829.jpg.png'
-import Img7 from '../img/homepage-images/IMG_3831.jpg.png'
-import Img8 from '../img/homepage-images/IMG_4688.jpg.png'
-import Img9 from '../img/homepage-images/IMG_8489.jpg.png'
-import Img10 from '../img/homepage-images/IMG_8937.jpg.png'
-import Img11 from '../img/homepage-images/balacalva.tiff.png'
-import Img12 from '../img/homepage-images/hemp bonnet.tiff.png'
-import Img13 from '../img/homepage-images/IMG_1640.jpg.png'
-import Img14 from '../img/homepage-images/Hyères_Evade_2021 (1).tiff.png'
-import Img15 from '../img/homepage-images/evadehouse_29_01_20214877.jpg.png'
-import Img16 from '../img/homepage-images/evadehouse_29_01_20215140.jpg.png'
-import Img17 from '../img/homepage-images/evadehouse_29_01_20215227.jpg.png'
-import Img18 from '../img/homepage-images/0fec83eb-6f78-4d43-bdcc-4ace622692fb.jpg.png'
-import Img19 from '../img/homepage-images/9BE62C14-75BB-49D1-953E-3C0444479AA4.jpg.png'
-import Img20 from '../img/homepage-images/E5059BDD-787C-4219-BC61-6A74F4FB31E8.jpg.png'
-
-
-
-
+import Img1 from "../img/homepage-images/3.tiff.png";
+import Img2 from "../img/homepage-images/cowl.tiff.png";
+import Img3 from "../img/homepage-images/veil.tiff.png";
+import Img4 from "../img/homepage-images/IMG_1640.jpg.png";
+import Img5 from "../img/homepage-images/IMG_1722.jpg.png";
+import Img6 from "../img/homepage-images/IMG_3829.jpg.png";
+import Img7 from "../img/homepage-images/IMG_3831.jpg.png";
+import Img8 from "../img/homepage-images/IMG_4688.jpg.png";
+import Img9 from "../img/homepage-images/IMG_8489.jpg.png";
+import Img10 from "../img/homepage-images/IMG_8937.jpg.png";
+import Img11 from "../img/homepage-images/balacalva.tiff.png";
+import Img12 from "../img/homepage-images/hemp bonnet.tiff.png";
+import Img13 from "../img/homepage-images/IMG_1640.jpg.png";
+import Img14 from "../img/homepage-images/Hyères_Evade_2021 (1).tiff.png";
+import Img15 from "../img/homepage-images/evadehouse_29_01_20214877.jpg.png";
+import Img16 from "../img/homepage-images/evadehouse_29_01_20215140.jpg.png";
+import Img17 from "../img/homepage-images/evadehouse_29_01_20215227.jpg.png";
+import Img18 from "../img/homepage-images/0fec83eb-6f78-4d43-bdcc-4ace622692fb.jpg.png";
+import Img19 from "../img/homepage-images/9BE62C14-75BB-49D1-953E-3C0444479AA4.jpg.png";
+import Img20 from "../img/homepage-images/E5059BDD-787C-4219-BC61-6A74F4FB31E8.jpg.png";
 
 export const HomepageImages: FunctionComponent = () => {
-  return <ImageGrid>
-    <img src={Img1} />
-    <img src={Img2} />
-    <img src={Img3} />
-    <img src={Img4} />
-    <img src={Img5} />
-    <img src={Img6} />
-    <img src={Img7} />
-    <img src={Img8} />
-    <img src={Img9} />
-    <img src={Img10} />
-    <img src={Img11} />
-    <img src={Img12} />
-    <img src={Img13} />
-    <img src={Img14} />
-    <img src={Img15} />
-    <img src={Img16} />
-    <img src={Img17} />
-    <img src={Img18} />
-    <img src={Img19} />
-    <img src={Img20} />
-  </ImageGrid>
-}
+  return (
+    <ImageGrid>
+      <img src={Img1} />
+      <img src={Img2} />
+      <img src={Img3} />
+      <img src={Img4} />
+      <img src={Img5} />
+      <img src={Img6} />
+      <img src={Img7} />
+      <img src={Img8} />
+      <img src={Img9} />
+      <img src={Img10} />
+      <img src={Img11} />
+      <img src={Img12} />
+      <img src={Img13} />
+      <img src={Img14} />
+      <img src={Img15} />
+      <img src={Img16} />
+      <img src={Img17} />
+      <img src={Img18} />
+      <img src={Img19} />
+      <img src={Img20} />
+    </ImageGrid>
+  );
+};
 
 export default HomepageImages;
